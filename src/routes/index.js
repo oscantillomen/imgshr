63b7e49c52c2f51610cfdd68d549d057e46e3078
oscantillomen <oscantillomen@gmail.com>
2019-11-19 17:43:30 -0500

    Show images in view

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const home = require('../controllers/home');
-const image = require('../controllers/image');
+const home = require("../controllers/home");
+const image = require("../controllers/image");
 
 module.exports = app => {
+  app.get("/", home.index);
 
-    app.get('/', home.index);
+  app.get("/images", image.images);
+  app.get("/images/:image_id", image.index);
+  app.post("/images", image.create);
+  app.post("/images/:image_id/like", image.like);
+  app.post("/images/:image_id/comment", image.comment);
+  app.delete("/images/:image_id", image.remove);
 
-    app.get('/images', image.images);
-    app.get('/images/:image_id', image.index);
-    app.post('/images', image.create);
-    app.post('/images/:image_id/like', image.like);
-    app.post('/images/:image_id/comment', image.comment);
-    app.delete('/images/:image_id', image.remove);
-
-    app.use(router);
-}
+  app.use(router);
+};
