d996ea6b5c13766a432432f34e14144d4077aba0
oscantillomen <oscantillomen@gmail.com>
2019-11-20 00:28:42 -0500

    Working on delete post

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -8,9 +8,9 @@ const { Image, Comment } = require('../models');
 const ctrl = {};
 
 ctrl.index = async (req, res) => {
-  const viewModel = { image: {}, comments: {}};
+  const viewModel = { image: {}, comments: {} };
 
-  const image = await Image.findOne({ filename: {$regex: req.params.image_id} });
+  const image = await Image.findOne({ filename: { $regex: req.params.image_id } });
   if (image) {
     image.views = image.views + 1;
     viewModel.image = image;
@@ -22,59 +22,59 @@ ctrl.index = async (req, res) => {
 };
 
 ctrl.images = async (req, res) => {
-    res.json({ msg: "Image uploaded!" });
-    
+  res.json({ msg: "Image uploaded!" });
+
 };
 
 ctrl.create = (req, res) => {
-    const saveImage = async () => {
-        const imgUrl = randomNumber();
-        const images = await Image.find({ filename: imgUrl });
-        if(images.length > 0) saveImage();
-        else{
-            const imageTempPath = req.file.path;
-            const ext = path.extname(req.file.originalname).toLowerCase();
-            const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
-            if (
-              ext === ".png" ||
-              ext === ".jpg" ||
-              ext === ".jpeg" ||
-              ext === ".gif"
-            ) {
-              try {
-                await fs.rename(imageTempPath, targetPath);
-                const newImg = new Image({
-                  title: req.body.title,
-                  filename: imgUrl + ext,
-                  description: req.body.description
-                });
-                const imageSaved = await newImg.save();
-                res.redirect("/images/" + imgUrl);
-              } catch (error) {
-                await fs.unlink(imageTempPath);
-                res.send(error);
-              }
-            } else {
-              await fs.unlink(imageTempPath);
-              res.status(500).json({ error: "Only images are allowed" });
-            }
+  const saveImage = async () => {
+    const imgUrl = randomNumber();
+    const images = await Image.find({ filename: imgUrl });
+    if (images.length > 0) saveImage();
+    else {
+      const imageTempPath = req.file.path;
+      const ext = path.extname(req.file.originalname).toLowerCase();
+      const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
+      if (
+        ext === ".png" ||
+        ext === ".jpg" ||
+        ext === ".jpeg" ||
+        ext === ".gif"
+      ) {
+        try {
+          await fs.rename(imageTempPath, targetPath);
+          const newImg = new Image({
+            title: req.body.title,
+            filename: imgUrl + ext,
+            description: req.body.description
+          });
+          const imageSaved = await newImg.save();
+          res.redirect("/images/" + imgUrl);
+        } catch (error) {
+          await fs.unlink(imageTempPath);
+          res.send(error);
         }
-    };
-    saveImage();
+      } else {
+        await fs.unlink(imageTempPath);
+        res.status(500).json({ error: "Only images are allowed" });
+      }
+    }
+  };
+  saveImage();
 };
 
 ctrl.like = async (req, res) => {
-  const image = await Image.findOne({ filename: { $regex: req.params.image_id }});
+  const image = await Image.findOne({ filename: { $regex: req.params.image_id } });
   if (image) {
     image.likes = image.likes + 1;
     await image.save();
     res.json({ likes: image.likes });
-  } else res.status(500).json({error: 'Internal Error'});
+  } else res.status(500).json({ error: 'Internal Error' });
 };
 
 ctrl.comment = async (req, res) => {
-  const image = await Image.findOne({ filename: {$regex: req.params.image_id }});
-  if(image){
+  const image = await Image.findOne({ filename: { $regex: req.params.image_id } });
+  if (image) {
     const newComment = new Comment(req.body);
     newComment.gravatar = md5(newComment.email);
     newComment.image_id = image._id;
@@ -83,8 +83,15 @@ ctrl.comment = async (req, res) => {
   } else res.redirect('/');
 };
 
-ctrl.remove = (req, res) => {
-  res.send("Index page");
+ctrl.remove = async (req, res) => {
+  const image = await Image.findOne({ filename: { $regex: req.params.image_id }})
+  
+  if (image) {
+    /*await fs.unlink(path.resolve(`./src/public/upload/${image.filename}`));
+    await Comment.deleteOne({ image_id: image._id });
+    await image.remove();*/
+    res.json(true)
+  }
 };
 
 module.exports = ctrl;
